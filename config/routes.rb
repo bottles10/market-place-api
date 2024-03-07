aded330774eec8467028712cfaaee4957f57ca33
bottles10 <henryaovare@gmail.com>
2024-03-07 16:24:31 -0600

    Set the routes constraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  
+  # Api definition
+  namespace :api, defaults: { format: :json } do
+    # Listing resources here
+  end
+
 end
