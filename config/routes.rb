8385cd666bc899a71300eb8ff89bfb3bf7a71f1b
bottles10 <henryaovare@gmail.com>
2024-03-08 20:05:10 +0000

    add user create endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       # Listing resources here
-      resources :users, only: [:show]
+      resources :users, only: [:show, :create]
     end
   end
 end
