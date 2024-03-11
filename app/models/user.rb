6df94967e944872f007abcb9cfda814081fe6caa
bottles10 <henryaovare@gmail.com>
2024-03-11 22:14:06 +0000

    Generate product model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :products, dependent: :destroy
+  
   validates :email, presence: true, uniqueness: true, format: { with: /@/ }
   validates :password_digest, presence: true
 end
