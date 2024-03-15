03dc78ce5db6f1702d415cf91f18906d0eba0c78
bottles10 <henryaovare@gmail.com>
2024-03-15 23:19:56 +0000

    Add search scope on the product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,17 @@ class Product < ApplicationRecord
   validates :title, :user_id, presence: true
   validates :price, presence: true,  numericality: { greater_than_or_equal_to: 0 }
 
+  scope :filter_by_title, lambda { |keyword|
+    where('lower(title) LIKE ?', "%#{keyword.downcase}%") 
+  }
+  
+  scope :above_or_equal_to_price, lambda { |price|
+    where('price >= ?', price)
+  }
+
+  scope :below_or_equal_to_price, lambda { |price| 
+    where('price <= ?', price)
+  }
+
+  scope :recent, lambda { order(updated_at: :desc) }
 end
