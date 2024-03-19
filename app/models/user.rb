adfc1ac773d442ad10cfe63c09553d9964ccf064
bottles10 <henryaovare@gmail.com>
2024-03-19 16:07:58 +0000

    Generate orders

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :products, dependent: :destroy
+  has_many :orders, dependent: :destroy
   
   validates :email, presence: true, uniqueness: true, format: { with: /@/ }
   validates :password_digest, presence: true
