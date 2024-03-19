138bb2102d7cd9e0123bfb9af931c2c8bad0fb1a
bottles10 <henryaovare@gmail.com>
2024-03-19 23:07:07 +0000

    Add show action for order

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       # Listing resources here
-      resources :orders, only: [:index]
+      resources :orders, only: [:index, :show]
       resources :users, only: [:show, :create, :update, :destroy]
       resources :tokens, only: [:create]
       resources :products
