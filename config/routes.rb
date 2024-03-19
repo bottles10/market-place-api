6f4f440bb9bddefef9a19133fbc85c3a1a4121b8
bottles10 <henryaovare@gmail.com>
2024-03-19 23:01:11 +0000

    Add the index action for order

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       # Listing resources here
+      resources :orders, only: [:index]
       resources :users, only: [:show, :create, :update, :destroy]
       resources :tokens, only: [:create]
       resources :products
