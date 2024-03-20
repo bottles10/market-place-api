4e17cb8110220b69e3494c561972e74ff26e3241
bottles10 <henryaovare@gmail.com>
2024-03-20 21:26:17 +0000

    Add create method for orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       # Listing resources here
-      resources :orders, only: [:index, :show]
+      resources :orders, only: [:index, :show, :create]
       resources :users, only: [:show, :create, :update, :destroy]
       resources :tokens, only: [:create]
       resources :products
